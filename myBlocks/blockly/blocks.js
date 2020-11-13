bb05f41ce8bc8f5ca7a86cb4528996e4ee46db2d
hawan0228 <74400718+hawan0228@users.noreply.github.com>
2020-11-13 16:02:04 +0800

    Add files via upload

---
+++ myBlocks/blockly/blocks.js
@@ -0,0 +1,10 @@
+Blockly.Blocks['sayhello'] = {
+  init: function() {
+    this.appendDummyInput()
+        .appendField("哈囉");
+    this.setOutput(true, null);
+    this.setColour(230);
+ this.setTooltip("");
+ this.setHelpUrl("");
+  }
+};
